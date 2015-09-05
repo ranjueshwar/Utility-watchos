6e4665cc0cf4974a355efc20355f2fed5db2d7c5
Prathap Murthy <prathapmurthy@prathaps-macbook-pro.local>
2015-09-05 16:13:07 +0530

    Initial Commit

---
+++ Utility/Utility.h
@@ -0,0 +1,19 @@
+//
+//  Utility.h
+//  Utility
+//
+//  Created by Prathap Murthy on 05/09/15.
+//  Copyright © 2015 Prathap Murthy. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Utility.
+FOUNDATION_EXPORT double UtilityVersionNumber;
+
+//! Project version string for Utility.
+FOUNDATION_EXPORT const unsigned char UtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utility/PublicHeader.h>
+
+
